a8312db1e4dc87f7851cef36b3c13a0c3e8c8f5c
andy xie <andy.xning@gmail.com>
2016-11-25 21:04:26 +0800

    update comment

--- models/handler.go
+++ models/handler.go
@@ -3,9 +3,9 @@ package models
 import "k8s.io/kubernetes/pkg/api"
 
 type EventHandler interface {
-	// HandleEvent sinks specific event to sinks.
+	// HandleEvent sinks event.
 	HandleEvent([]Sink, *api.Event)
-	// AlertEventReason returns a unique string representing an alert event.
+	// AlertEventReason returns a unique string representing an alert event reason.
 	AlertEventReason() string
 	// Reason returns a string describing the event reason in short.
 	Reason() string
--- models/source.go
+++ models/source.go
@@ -3,7 +3,7 @@ package models
 import "k8s.io/kubernetes/pkg/api"
 
 type Source interface {
-	// Start starts to collect event and send it to the eventChan.
+	// Start starts to collect event and send it to eventChan.
 	Start(eventChan chan<- *api.Event)
 	// Stop stops Source gracefully.
 	Stop()
